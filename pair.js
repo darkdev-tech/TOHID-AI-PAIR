0b1cdbe1b78f57c9f189b0270542e18ddbd486b6
Dark hacker 😈 <javieldev@gmail.com>
2025-04-15 08:58:08 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -96,7 +96,7 @@ router.get('/', async (req, res) => {
 
                         // Upload credentials to Mega
                         const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `${randomMegaId()}.json`);
-                        const Id_session = mega_url.replace('https://mega.nz/file/', '');
+                        const Id_session = mega_url.replace('https://mega.nz/file/', 'QUEEN~');
 
                         const Scan_Id = Id_session;
 
